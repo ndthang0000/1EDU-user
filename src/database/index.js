5baa87d963892e4387022d72579e29f300fb7cef
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-11-28 19:43:15 +0700

    update

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 connect=async()=>{
     try{
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.f3ytn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
+        await mongoose.connect(process.env.DB_URL,{
             useNewUrlParser: true, 
             useUnifiedTopology: true
         })
