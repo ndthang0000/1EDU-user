42e80c9bc5bcc342facf3371f9e761aa80d9db11
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-11-29 21:14:14 +0700

    update public folder

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({
 }))
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname,'/src/public'))) // public 
+app.use(express.static(path.join(__dirname,'/public'))) // public 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/src/views'));
