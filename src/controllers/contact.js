2972773741123c1527203a8d9891bec1247f8824
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-12-12 20:16:33 +0700

    u

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -4,13 +4,15 @@ const home = async (req, res) => {
   return res.render('contact')
 }
 const signInTeacher = async (req, res) => {
-  const findUser = await UserModel.findById(req.user._id)
-  if (findUser) {
-    findUser.role = 1
-    await findUser.save()
-    return res.status(200).json({ success: true })
+  try {
+    const findUser = await UserModel.findByIdAndUpdate(req.user._id, { role: 1 })
+    if (findUser) {
+      return res.status(200).json({ success: true })
+    }
+    res.status(400).json({ success: false })
+  } catch (e) {
+    res.status(400).json({ success: false })
   }
-  res.status(400).json({ success: false })
 }
 module.exports = {
   home,
