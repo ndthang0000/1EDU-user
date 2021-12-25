6d6b6a9942302149778756b27f0700436568c943
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-12-25 19:27:55 +0700

    finall

--- src/controllers/instructor.js
+++ src/controllers/instructor.js
@@ -1,9 +1,10 @@
 const { UserModel, CategoryModel } = require('../models')
+const { helper } = require('../helpers')
 
 const home = async (req, res) => {
   const category = await CategoryModel.find({})
   const allTeacher = await UserModel.find({ role: 1 }).limit(8)
-  res.render('instructor', { allTeacher, category })
+  res.render('instructor', { allTeacher, category, newLine: helper.newLinee })
 }
 
 module.exports = {
